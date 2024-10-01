c7558ff755c793963fdd5ac790262e1d20ac7fe0
ttyybb-code <128849581+ttyybb-code@users.noreply.github.com>
2024-09-30 19:15:19 -0600

    Added send/receave cna view files

--- client.py
+++ client.py
@@ -1,7 +1,73 @@
 import socket
+import pickle
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf8"))
+
+def receive_pickle(socket):
+    full_msg = b""
+    new_msg = True
+    msg_length = 0
+    while True:
+        msg = socket.recv(16)
+        if not msg:
+            print("The connection was closed by the server")
+            return None
+
+        if new_msg:
+            msg_length = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg
+
+        if len(full_msg) - HEADERSIZE == msg_length:
+            return pickle.loads(full_msg[HEADERSIZE:])
+
+
+def menu():
+    action = input("Download, upload, or view files: ").lower()
+    while True:
+        if action in ("download", "upload", "view", "view files", "quit"):
+            if action == "view files":
+                action = "view"
+            return action
+        else:
+            action = input(
+                "Please enter 'download', 'upload', 'view', or 'quit' ").lower()
+
+
+def send_msg(socket, msg):
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    msg = bytes(msg, "utf-8")
+    socket.send(msg)
+
+
+def main():
+    files = receive_pickle(s)
+    print("Available files:")
+
+    if files is None:
+        return
+    for file in files:
+        print(file)
+    while True:
+        action = menu()
+        send_msg(s, action)
+
+        if action == "quit":
+            print("going to quit")
+            s.close()
+            exit()
+        elif action == "view":
+            print("Available files:")
+            files = receive_pickle(s)
+            if files is None:
+                return
+            for file in files:
+                print(file)
+
+
+main()
--- server.py
+++ server.py
@@ -1,10 +1,56 @@
 import socket
+import pickle
+from os import listdir
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("0.0.0.0", 1234))
-s.listen(5)
+HEADERSIZE = 10
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", 1234))
+server.listen(1)
+
+
+def send_dir(socket, directory):
+    files = listdir(directory)
+    msg = pickle.dumps(files)
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
+    socket.send(msg)
+    return
+
+
+def receive_msg(socket):
+    full_msg = ""
+    new_msg = True
+    msg_length = 0
+    while len(full_msg) - HEADERSIZE != msg_length:
+        msg = socket.recv(16)
+        if not msg:
+            print("Connection has been terminated by the client")
+            return None
+
+        if new_msg:
+            msg_length = int(msg[:HEADERSIZE])
+            new_msg = False
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msg_length:
+            return full_msg[HEADERSIZE:]
+
 
 while True:
-    clientsocket, address = s.accept()
+    directory = "storage"
+    clientsocket, address = server.accept()
     print(f"Connection to {address} has been established")
-    clientsocket.send("echo".encode("utf-8"))
+    directory = "storage"
+    send_dir(clientsocket, directory)
+    while True:
+        action = receive_msg(clientsocket)
+        if action == "view":
+            send_dir(clientsocket, directory)
+        elif action == "download":
+            ...
+        elif action == "upload":
+            ...
+        elif action is None:
+            print(f"Connection with {address} has been terminated")
+            break
